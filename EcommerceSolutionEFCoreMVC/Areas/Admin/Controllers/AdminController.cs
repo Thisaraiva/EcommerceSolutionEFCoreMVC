0d09299d71c67a491d806863741e4bf6ede0e57d
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2024-10-29 22:51:22 -0300

    Implementation of controllers and views for roles and users

--- EcommerceSolutionEFCoreMVC/Areas/Admin/Controllers/AdminController.cs
+++ EcommerceSolutionEFCoreMVC/Areas/Admin/Controllers/AdminController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceSolutionEFCoreMVC.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         // GET: AdminController
---
+++ EcommerceSolutionEFCoreMVC/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,53 @@
+﻿using EcommerceSolutionEFCoreMVC.Models.Entities;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceSolutionEFCoreMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdminUsersController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = userManager.Users;
+            return View(users);
+        }
+
+        [HttpPost]        
+        public async Task<IActionResult> DeleteUser (string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErroMessage = $"User with Id = {id} not found!";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("Index");
+            }
+        }
+    }
+}
