c373de4676380ba0d745e1ab6d1d97fcd0d85c52
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2024-11-13 23:14:16 -0300

    Inclusion of the two-factor QR Code image and improvement on the my cart page.

--- EcommerceSolutionEFCoreMVC/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ EcommerceSolutionEFCoreMVC/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,7 +181,7 @@ namespace EcommerceSolutionEFCoreMVC.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("EcommerceEFCoreMVC"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
